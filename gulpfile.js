eee79f81a2bf7dcac4c47bf8ce3e5bdacff78936
Brian Ellis <brian@briansresume.com>
2017-02-07 13:35:18 -0500

    Gulp configuration of SASSDoc and task configured for watch and compilation

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 // grab our gulp packages
 var gulp  = require('gulp'),
     gutil = require('gulp-util'),
-    sass  = require('gulp-sass');
+    sass  = require('gulp-sass'),
+    sassdoc = require('sassdoc');
 
 // create a default task and just log a message
 gulp.task('default', function() {
@@ -12,12 +13,21 @@ gulp.task('default', function() {
 
 // SASS manual compile
 gulp.task('sass-styles', function () {
-	gulp.src('styles/scss/**/*.scss')
-		.pipe( sass().on('error', sass.logError) )
-		.pipe( gulp.dest('css/') );
+    gulp.src('styles/scss/**/*.scss')
+        .pipe( sass().on('error', sass.logError) )
+        .pipe( gulp.dest('css/') );
+});
+
+// SASSDoc compile
+gulp.task('sassdoc', function () {
+  return gulp.src('styles/scss/**/*.scss')
+    .pipe(sassdoc());
 });
 
 // SASS watch task
 gulp.task('sass-watch', function () {
-	gulp.watch('styles/scss/**/*.scss', ['sass-styles']);
+    gulp.watch('styles/scss/**/*.scss', ['sass-styles', 'sassdoc']);
 });
+
+// SASS manual build with doc
+gulp.task('sass-compile',['sass-styles', 'sassdoc']);
