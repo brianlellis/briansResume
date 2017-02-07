6753cdaa5bc2ec36d7c64f3386a7cb3a3233d9af
Brian Ellis <brian.ellis@blueacorn.com>
2017-02-07 11:37:18 -0500

    Rel a (#5)
    * configured variables and 2 sections of information for homepage
    
    * Added media directories for homepage, updated gitignore to not track system files, still need to finish media for clients directory
    
    * Added in award and client photos to website site and alt, ADA tags
    
    * Configured gulp to use sass compile and sass watch, set up base and header basic styles
    
    * Finished base homepage design

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,22 @@
 
 // grab our gulp packages
 var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    sass  = require('gulp-sass');
 
 // create a default task and just log a message
 gulp.task('default', function() {
   return gutil.log('Gulp is running!')
+});
+
+// SASS manual compile
+gulp.task('sass-styles', function () {
+	gulp.src('styles/scss/**/*.scss')
+		.pipe( sass().on('error', sass.logError) )
+		.pipe( gulp.dest('css/') );
+});
+
+// SASS watch task
+gulp.task('sass-watch', function () {
+	gulp.watch('styles/scss/**/*.scss', ['sass-styles']);
 });
