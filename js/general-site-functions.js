b278d3330c3834442530e69aa9364dc62e62a968
Brian Ellis <brian@briansresume.com>
2017-02-09 06:55:19 -0500

    Fnished site javascript for modal

--- js/general-site-functions.js
+++ js/general-site-functions.js
@@ -1,7 +1,12 @@
 var siteCore = siteCore || {}; // Check if object is present first
 
 siteCore.modal = (function ($) {
-  var _self; // Setting up a shared private var
+  var _self, // Setting up a shared private var for this object ref
+      mainNav = $('body > nav .modal'), // Website top nav
+      modal = $('#modal-overlay'), // Black modal overlay 
+      modalBox = $('#modal-overlay .modal-box'), // Modal box on top of container
+      contactSect = $('#contact-me'), // modal box contact section
+      resumeSect = $('#my-resume');  // modal box resume section
 
   return {
     init: function () {
@@ -10,13 +15,21 @@ siteCore.modal = (function ($) {
       _self.observers();
     },
     observers: function () {
-      var mainNav = $('body > nav .modal'),
-          modal = $('#modal-overlay');
+      _self.modalOpenClose();
+      _self.modalNavWatch();
+    },
+    modalOpenClose: function () {
       // Navigation menu click
       mainNav.click(function(event) {
         modal.css('top', 0).addClass('active');
 
-        $(this).hasClass('resume') ? $('#modal-overlay .resume').addClass('active') : $('#modal-overlay .contact').addClass('active');
+        if ($(this).hasClass('resume')) { 
+          $('#modal-overlay .resume').addClass('active');
+          resumeSect.show();
+        } else {
+          $('#modal-overlay .contact').addClass('active');
+          contactSect.show();
+        }
       });
 
       // Modal overlay click to close
@@ -24,6 +37,28 @@ siteCore.modal = (function ($) {
         if ($(this).hasClass('active')) {
           $(this).css('top', '-100%');
           $('#modal-overlay .active').removeClass('active');
+          contactSect.hide();
+          resumeSect.hide();
+        }
+      });
+    },
+    modalNavWatch: function () {
+      var modalNav = $('#modal-overlay nav li');
+      modalBox.click(function(event) {
+        event.stopPropagation();
+      });
+      modalNav.click(function(event) {
+        if (! $(this).hasClass('active')) {
+          $('#modal-overlay .active').removeClass('active');
+          $(this).addClass('active');
+
+          if ($('#modal-overlay .active').hasClass('resume')) {
+            contactSect.hide();
+            resumeSect.show();
+          } else {
+            contactSect.show();
+            resumeSect.hide();
+          }
         }
       });
     }
