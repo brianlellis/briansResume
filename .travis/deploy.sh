cfa72f45ee8b20b6141a2848e6e0b9503844a005
root <root@personal-website-2017.localdomain>
2017-02-10 19:33:33 +0000

    Revert and added back in ssh-add

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,9 +4,9 @@ echo "I DID RUN!!!!"
 
 set -xe # print exit on first failure
 
-if [ $TRAVIS_BRANCH == 'staging' ]; then
+if [ "$TRAVIS_BRANCH" == 'staging' ]; then
 	eval "$(ssh-agent -s)" # start ssh agent
-	#ssh-add ~/.ssh/travis_rsa ##commented out due to key already being added
+	ssh-add ~/.ssh/travis_rsa 
 	git remote add deploy "travis@briansresume.com:/var/www/html" # deploy to html default
 	git config user.name "TravisCI"
 	git config user.email "travisdeploy@briansresume.com"git push -f deploy HEAD:staging # exec deploy method
