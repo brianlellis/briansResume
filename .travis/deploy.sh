d5e80dd8d58be7797648cbc5d893bb98014d8cfb
root <root@personal-website-2017.localdomain>
2017-02-10 19:26:08 +0000

    remove add key cmd from deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,7 +6,7 @@ set -xe # print exit on first failure
 
 if [ "$TRAVIS_BRANCH" == 'staging' ]; then
 	eval "$(ssh-agent -s)" # start ssh agent
-	ssh-add ~/.ssh/travis_rsa
+	#ssh-add ~/.ssh/travis_rsa ##commented out due to key already being added
 	git remote add deploy "travis@briansresume.com:/var/www/html" # deploy to html default
 	git config user.name "TravisCI"
 	git config user.email "travisdeploy@briansresume.com"git push -f deploy HEAD:staging # exec deploy method
