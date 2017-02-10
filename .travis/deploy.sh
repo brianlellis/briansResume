ade255d092ba1da1422ddefe8ce92ee5c1628687
root <root@personal-website-2017.localdomain>
2017-02-10 20:24:57 +0000

    Added sudo to chmod for deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,7 +6,8 @@ set -xe # print exit on first failure
 
 if [ "$TRAVIS_BRANCH" == 'staging' ]; then
         #ls -al #show pwd 
-	eval "$(ssh-agent -s)" # start ssh agent
+	sudo chmod 600 travis_rsa.ence # force correct file perm, this was done with http://askubuntu.com/questions/147241/execute-sudo-without-password
+        eval "$(ssh-agent -s)" # start ssh agent
 	ssh-add travis_rsa 
 	git remote add deploy "travis@briansresume.com:/var/www/html" # deploy to html default
 	git config user.name "TravisCI"
