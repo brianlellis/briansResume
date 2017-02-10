06ca3c8bc7f265cbb90fef5c295550dba881bf85
root <root@personal-website-2017.localdomain>
2017-02-10 21:02:26 +0000

    file perm fix

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,7 +7,7 @@ set -xe # print exit on first failure
 if [ "$TRAVIS_BRANCH" == 'staging' ]; then
         #ls -al #show pwd 
         eval "$(ssh-agent -s)" # start ssh agent
-        ssh-add travis_rsa 
+        ssh-add ~/.ssh/travis_rsa 
 	git remote add deploy "travis@briansresume.com:/var/www/html" # deploy to html default
 	git config user.name "TravisCI"
 	git config user.email "travisdeploy@briansresume.com"git push -f deploy HEAD:staging # exec deploy method
