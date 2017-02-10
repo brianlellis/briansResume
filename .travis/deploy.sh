0a1e548fcbaa5406650d958dd36437b39f7ead6c
root <root@personal-website-2017.localdomain>
2017-02-10 20:03:36 +0000

    Fix rel file path for travis_rsa ssh-add:

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,9 +5,9 @@ echo "I DID RUN!!!!"
 set -xe # print exit on first failure
 
 if [ "$TRAVIS_BRANCH" == 'staging' ]; then
-        ls -al #show pwd 
+        #ls -al #show pwd 
 	eval "$(ssh-agent -s)" # start ssh agent
-	ssh-add ../travis_rsa 
+	ssh-add travis_rsa 
 	git remote add deploy "travis@briansresume.com:/var/www/html" # deploy to html default
 	git config user.name "TravisCI"
 	git config user.email "travisdeploy@briansresume.com"git push -f deploy HEAD:staging # exec deploy method
