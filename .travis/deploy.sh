5accb7e676114bbf15da7494cc2b97c042076e3f
root <root@personal-website-2017.localdomain>
2017-02-10 20:58:49 +0000

    removed sudo calls

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,7 +6,6 @@ set -xe # print exit on first failure
 
 if [ "$TRAVIS_BRANCH" == 'staging' ]; then
         #ls -al #show pwd 
-	sudo chmod 600 travis_rsa # force correct file perm, this was done with http://askubuntu.com/questions/147241/execute-sudo-without-password
         eval "$(ssh-agent -s)" # start ssh agent
         ssh-add travis_rsa 
 	git remote add deploy "travis@briansresume.com:/var/www/html" # deploy to html default
