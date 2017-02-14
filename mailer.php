c2706f2cb566225420c1925f6e6562bd7296f821
Brian Ellis <brian@briansresume.com>
2017-02-13 19:38:07 -0500

    test 2

--- mailer.php
+++ mailer.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "hello@example.com";
+        $recipient = "brian@briansresume.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
