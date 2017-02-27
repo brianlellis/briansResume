c35b922b6f053a967e8a1aeb553653d822ce8653
Brian Ellis <brian@briansresume.com>
2017-02-27 17:01:38 -0500

    Removed proven section and integrated to index modal page

--- js/general-site-functions.js
+++ js/general-site-functions.js
@@ -1,7 +1,11 @@
+/**
+ * While it's common understanding to never make a god object in programming the script is so small
+ * it made more sense for quick reference to build this as a dynamic object creation based on an href eval
+ */
+
 var siteCore = (function ($) {
   var _self, // Setting up a shared private var for this object ref
-      pageURL = window.location.href, // Get current page's url
-      mainNav = $('body > nav .modal'), // Website top nav
+      modalNav = $('.modal'), // Modal activation event
       modal = $('#modal-overlay'), // Black modal overlay 
       modalBox = modal.find('.modal-box'), // Modal box on top of container
       contactForm = modal.find('form'),
@@ -9,172 +13,108 @@ var siteCore = (function ($) {
       contactStatus = modal.find('.status'),
 
       // Index page selectors
-      clientPictures,
-      slideCount, 
-      slideWidth, 
-      slideHeight,
+      slideCount, slideWidth, slideHeight,
       sliderUlWidth,
       autoSlide,
-      autoSlideStopped,
-      siteResourceSection,
-      resourceImages,
-
-      // Projects page selectors
-      a,
-
-      // About page selectors
-      b,
-
-      // Learning page selectors
-      c;
+      siteResourcSect,
+      resourceImages;
 
   return {
     init: function () {
       _self = this;
-
       _self.observers(); // General site observers
 
-      /** 
-       * PAGE SPECIFIC FUNCTIONS
-      **/
-      // about
-      if (pageURL.indexOf("about") > 0) {
-        carouselHolder = $('carouselHolder');
-
-        _self.infiniteCarousel();
-      } 
-      // learning
-      else if (pageURL.indexOf("learning") > 0) {
-
-      } 
-      // past projects
-      else if (pageURL.indexOf("past-projects") > 0) {
-      } 
       // default for index page
-      else {
-        awardCards = $('#awards-and-certs');
-        clientPictures = $('main section.clients');
-        siteResourceSection = $('#site-resources');
-        resourceImages = $('#site-resources .placard');
+      awardCards = $('#awards-and-certs');
+      siteResourcSect = $('#site-resources');
+      resourceImages = $('#site-resources .placard');
 
-        _self.awardsAndCerts(); // awards and certs flipcard
-        _self.goToClients(); // goes to clients page for client grid
-        _self.resourceShowcase(); // bottom section resources
-        _self.resourceImgSpin(); // smooth spin animation
-        _self.freezeScroll(); // freezes scroll without using CSS
-
-        $(window).resize(function(event) {
-          _self.awardsAndCertsSizing();
-        });
-      }
+      _self.awardsAndCerts(); // awards and certs flipcard
+      _self.resourceShowcase(); // bottom section resources
+      _self.resourceImgSpin(); // smooth spin animation
+      _self.freezeScroll(); // freezes scroll without using CSS
     },
     observers: function () {
       _self.modalOpenClose();
       _self.modalContact();
+      $(window).resize(function(event) {
+        _self.awardsAndCertsSizing();
+      });
     },
     modalOpenClose: function () {
       // Navigation menu click
-      mainNav.click(function(event) {
+      modalNav.click(function(e) {
+        e.preventDefault();
+        if ( $(this).hasClass('proven') ) {
+          $('#proven-modal').show();
+          $('#contact-me').hide();
+        } else {
+          $('#proven-modal').hide();
+          $('#contact-me').show();
+        }
         modal.addClass('active');
       });
 
       // Modal overlay click to close
       modal.click(function(event) {
-        if ($(this).hasClass('active')) {
-          modal.removeClass('active');
-        }
+        if ( $(this).hasClass('active') ) modal.removeClass('active');
       });
       modalBox.click(function(event) {
         event.stopPropagation();
       });
     },
     modalContact: function () {
-      // Get the form.
-      var form = $('#ajax-contact');
-
-      // Get the messages div.
-      var formMessages = $('#form-messages');
+      var form = $('#ajax-contact'), // Get the form
+          formMsgs = $('#form-messages'); // Get the messages div
 
       // Set up an event listener for the contact form.
-      $(form).submit(function(event) {
-          // Stop the browser from submitting the form.
-          event.preventDefault();
+      form.submit(function(event) {
+        event.preventDefault(); // Stop the browser from submitting the form
+        var formData = form.serialize(); // Serialize the form data
 
-          // Serialize the form data.
-          var formData = $(form).serialize();
-
-          // Submit the form using AJAX.
-          $.ajax({
-              type: 'POST',
-              url: $(form).attr('action'),
-              data: formData
-          }).done(function(response) {
-              // Make sure that the formMessages div has the 'success' class.
-              $(formMessages).removeClass('error');
-              $(formMessages).addClass('success');
-              $(form).hide();
-
-              // Set the message text.
-              $(formMessages).text(response);
-
-              // Clear the form.
-              $('#email').val('');
-              $('#message').val('');
-          }).fail(function(data) {
-              // Make sure that the formMessages div has the 'error' class.
-              $(formMessages).removeClass('success');
-              $(formMessages).addClass('error');
-
-              // Set the message text.
-              if (data.responseText !== '') {
-                  $(formMessages).text(data.responseText);
-              } else {
-                  $(formMessages).text('Oops! An error occured and your message could not be sent.');
-              }
-          });
+        // Submit the form using AJAX.
+        $.ajax({
+            type: 'POST',
+            url: form.attr('action'),
+            data: formData
+        }).done(function(response) {
+            formMsgs.removeClass('error').addClass('success'); // Make 'success' class
+            form.hide();
+            formMsgs.text(response); // Set the message text
+            $('#email, #message').val(''); // Clear the form
+        }).fail(function(data) {
+          formMsgs.removeClass('success').addClass('error'); // Make 'error' class
+          // Set the message text
+          (data.responseText !== '') ?  formMsgs.text(data.responseText) :
+                                        formMsgs.text('Oops! An error stopped your message.');
+        });
       });
     },
-    /**
-      * index page functions
-    **/
     awardsAndCerts: function () {
       _self.awardsAndCertsSizing();
       _self.autoSlide();
     },
     awardsAndCertsSizing: function () {
       $('#sliderHold, #sliderHold li').css('width',''); // clear inline vals to allow css to give correct val
-      slideWidth = $('#sliderHold li').width(); // get initial value of li before slide design, val fed from scss partial _card-grid.scss
-      
+      slideWidth = $('#sliderHold li').width(); // get init val of li before slide design, val fed from scss partial _card-grid.scss  
       $('#sliderHold').width($('#sliderHold li').length * slideWidth); // made ul wide enough to go past overflow parent container
       $('#sliderHold li').width(slideWidth); // overwrite css % val to prev recorded val
     },
     autoSlide: function () {
       // Sets auto slide interval
       autoSlide = setInterval(function () {
-        if (! autoSlideStopped) {
           $('#sliderHold li:eq(0)').animate({
               marginLeft: - slideWidth * 1.2 // Multiplier ex 1.2 affects amount of slides to go past
           }, 'slow', function () {
               $('#sliderHold li:first-child').css('marginLeft','').appendTo('#sliderHold');
           });
-        } else {
-          setTimeout(function () {
-            autoSlideStopped = false;
-          }, 10000);
-        }
       }, 5000);
-    },
-    goToClients: function () {
-      clientPictures.click(function(event) {
-        window.location.href = "past-projects.html";
-      });
     },
     resourceShowcase: function () {
       var curScroll; // kept out of click closure so its not recommited to memory each time
 
-      siteResourceSection.click(function () {
+      siteResourcSect.click(function () {
         if (! $(this).hasClass('active') && ! $(this).hasClass('wait') ) {
-
           curScroll = $(document).scrollTop(); // get cur scroll val
 
           // smooth animation to cover screen
@@ -207,7 +147,7 @@ var siteCore = (function ($) {
     },
     resourceImgSpin: function () {
       resourceImages.hover(function() {
-        if (!$(this).hasClass('spinning') && !siteResourceSection.hasClass('active')) {
+        if (!$(this).hasClass('spinning') && !siteResourcSect.hasClass('active')) {
           $(this).addClass('spinning');
           _self.waiter($(this), 'spinning', 1000);
         } 
@@ -227,16 +167,10 @@ var siteCore = (function ($) {
     freezeScroll: function () {
       $('body').on({
           'mousewheel': function(e) {
-            if (siteResourceSection.hasClass('active') || siteResourceSection.hasClass('wait') ) {
-              e.preventDefault();
-              e.stopPropagation();
-            }
+            if (siteResourcSect.hasClass('active') || siteResourcSect.hasClass('wait') ) e.preventDefault();
           }
       })
     }
-    /**
-      * about page functions
-    **/
   }
 })(jQuery);
 
